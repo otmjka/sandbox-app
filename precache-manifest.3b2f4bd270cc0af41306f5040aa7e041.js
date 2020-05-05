0adb0310c743e18ce1de024459b27eb1440a2d45
otmjka <otmjka@gmail.com>
2020-05-05 17:08:04 +0500

    Updates

---
+++ precache-manifest.3b2f4bd270cc0af41306f5040aa7e041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cf55909ad22bc7d5f725269a179a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "510d2d3b9a71b69fedad",
+    "url": "/static/css/main.36715d8d.chunk.css"
+  },
+  {
+    "revision": "546f12c94d8e8cf7acc2",
+    "url": "/static/js/2.5282fa06.chunk.js"
+  },
+  {
+    "revision": "28164e9e1f7b49ce7422bf240655ba5b",
+    "url": "/static/js/2.5282fa06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510d2d3b9a71b69fedad",
+    "url": "/static/js/main.17bd9461.chunk.js"
+  },
+  {
+    "revision": "3339d4c18f84bc09389a",
+    "url": "/static/js/runtime-main.d00c6cf3.js"
+  }
+]);
