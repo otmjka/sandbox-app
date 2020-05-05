a7fec0e91fbad415de1f26c5a2b0b0c8326a3239
otmjka <otmjka@gmail.com>
2020-05-05 16:04:28 +0500

    welcome page

--- src/components/Common/WelcomeScreen/WelcomeScreen.tsx
+++ src/components/Common/WelcomeScreen/WelcomeScreen.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {useIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 import { useForm } from 'react-hook-form';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 import Layout from '../../Common/Layout';
 
@@ -10,11 +12,22 @@ import useStyles from './styles';
 
 export default function HomeScreen() {
   const classes = useStyles();
-  const {formatMessage} = useIntl()
+  const { formatMessage } = useIntl();
   return (
     <>
       <Layout type="home">
-        <Typography variant="h2">{formatMessage(messages.headerMsg)}</Typography>
+        <Grid container>
+          <Grid item xs={12}>
+            <Box display="flex" justifyContent="center" pt={8} mb={2}>
+              <Typography variant="h2">
+                {formatMessage(messages.headerMsg)}
+              </Typography>
+            </Box>
+            <Box display="flex" justifyContent="center" mb={2}>
+              <Typography>{formatMessage(messages.bodyMsg)}</Typography>
+            </Box>
+          </Grid>
+        </Grid>
       </Layout>
     </>
   );
--- src/components/Common/WelcomeScreen/messages.js
+++ src/components/Common/WelcomeScreen/messages.js
@@ -5,4 +5,8 @@ export default defineMessages({
     id: 'Common.WelcomeScreen.headerMsg',
     defaultMessage: 'Welcome!'
   },
+  bodyMsg: {
+    id: 'Common.WelcomeScreen.headerMsg',
+    defaultMessage: 'Parrot Wings is superb place where it can easy transfer your money! Let`s login to start'
+  }
 });
